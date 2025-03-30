166a1b2f57c906f621a9dc9832d7571867525907
Jose Luis Zanotti <joseluiszanotti@gmail.com>
2025-03-30 17:12:54 -0300

    update

--- cliente_epson-900fa_odoo-argentina/Linux/Printer/DriverFiscal.py
+++ cliente_epson-900fa_odoo-argentina/Linux/Printer/DriverFiscal.py
@@ -1,48 +1,29 @@
 
-from ctypes import byref, c_int, c_char, c_long, c_short, create_string_buffer, cdll
-import binascii
-import sys
-
-
-
+from ctypes import cdll, windll
 import platform
 import os
+
 EpsonLibInterface = False
+
 def get_driver():
-	try:
-		
-	
-		name_os = platform.system()
-		path  = os.path.dirname(os.path.realpath(__file__))
-		#print ('path library: '), path
-		archbits = platform.architecture()[0]
-		if name_os == 'Linux':
-			
-			
-			
-			if archbits[0:2] == "64":
-				fullpath = path + '/Library/Linux/64/libEpsonFiscalInterface.so'
-			else:
-				fullpath = path + '/Library/Linux/32/libEpsonFiscalInterface.so'
+    try:
+        name_os = platform.system()
+        path  = os.path.dirname(os.path.realpath(__file__))
+        archbits = platform.architecture()[0]
+        if name_os == 'Linux':
+            if archbits[0:2] == "64":
+                fullpath = path + '/Library/Linux/64/libEpsonFiscalInterface.so'
+            else:
+                fullpath = path + '/Library/Linux/32/libEpsonFiscalInterface.so'
+            EpsonLibInterface = cdll.LoadLibrary(fullpath)
+        else:
+            #Ojo: Siempre se va a leer la dll de 32 bits, porque se esta utilizando el python de 32 bits
+            '''if archbits[0:2] == "64":
+                fullpath =  path + "\\Library\\Windows\\64\\EpsonFiscalInterface.dll"
+            else:'''
+            fullpath =  path + "\\Library\\Windows\\32\\EpsonFiscalInterface.dll"
+            EpsonLibInterface = windll.LoadLibrary(fullpath)
+    except Exception as e:
+        raise Exception(e)
 
-			#print('fullpath: ' , fullpath)
-		
-			import ctypes
-			EpsonLibInterface = ctypes.cdll.LoadLibrary(fullpath)
-		
-		else:
-			from ctypes import windll
-			#Ojo: Siempre se va a leer la dll de 32 bits, porque se esta utilizando el python de 32 bits
-			'''if archbits[0:2] == "64":
-				fullpath =  path + "\\Library\\Windows\\64\\EpsonFiscalInterface.dll"
-			else:'''
-			print('fullpath: ')
-			fullpath =  path + "\\Library\\Windows\\32\\EpsonFiscalInterface.dll"
-			print('fullpath: ' , fullpath)
-			
-			EpsonLibInterface = windll.LoadLibrary(fullpath)
-	except Exception as e:	
-		raise Exception(e)
-
-	return EpsonLibInterface
-
+    return EpsonLibInterface
